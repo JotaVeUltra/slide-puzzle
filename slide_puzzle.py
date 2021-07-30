8969986164d9d22c1019afb458c1ee4e3cfd0ea3
João Marques <jotaveultra@gmail.com>
2021-07-30 16:37:52 -0300

    Create slide_puzzle.py

---
+++ slide_puzzle.py
@@ -0,0 +1,24 @@
+# TODO:
+# init pygame
+# check number of images inside the folder
+# if there are more than one image ask the player to choose
+# load the image and split it in tiles by the BOARDSIZE² number
+# randomize the tiles and check if solvable
+# game loop
+#    check for player actions
+#        mouse click or arrows pressed
+#    handle events
+#        check if tile can be moved
+#        change status of tile moved tile
+#        checke if player win
+#    update screen
+#        drawn board
+#        drawn tiles animation
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
